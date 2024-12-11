73781a6bb7d68e4c110049239dfa2586e9689cbc
mthcht <mathieuchotplassot@gmail.com>
2024-12-11 11:40:51 +0100

    Update fetch_IP_ranges_of_bad_ASN.py

--- Lists/ASNs/fetch_IP_ranges_of_bad_ASN.py
+++ Lists/ASNs/fetch_IP_ranges_of_bad_ASN.py
@@ -2,21 +2,31 @@ import csv
 import subprocess
 import os
 
-# Path to the CSV file
-csv_file_path = 'latest_bad_asn_phishing_list.csv'
+# Paths to the CSV files
+latest_csv_file_path = 'latest_bad_asn_phishing_list.csv'
+static_csv_file_path = 'bad_asn_static_list.csv'
 
 # Path to the script to be executed using relative path
 script_path = os.path.join('..', 'Ranges_IP_Address_Company_List', 'bgp.he.net', 'get_ip_range.py')
 
-# Read the AS numbers from the CSV file
-as_numbers = []
-with open(csv_file_path, mode='r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
-        as_numbers.append('AS' + row['as_number'])
+# Function to read AS numbers from a CSV file
+def read_as_numbers(csv_file_path):
+    as_numbers = []
+    with open(csv_file_path, mode='r', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            as_numbers.append('AS' + row['as_number'])
+    return as_numbers
+
+# Read AS numbers from both files
+latest_as_numbers = read_as_numbers(latest_csv_file_path)
+static_as_numbers = read_as_numbers(static_csv_file_path)
+
+# Combine AS numbers from both lists
+combined_as_numbers = list(set(latest_as_numbers + static_as_numbers))
 
 # Join the AS numbers into a single string separated by commas
-as_numbers_str = ','.join(as_numbers)
+as_numbers_str = ','.join(combined_as_numbers)
 
 # Construct the command
 command = f'python {script_path} -list {as_numbers_str} -format csv'
