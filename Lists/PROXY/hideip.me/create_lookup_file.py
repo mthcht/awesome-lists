a4baa6fc5fe9a80fb4b45086054f293725fb8965
mthcht <mathieuchotplassot@gmail.com>
2025-02-01 01:31:02 +0100

    Update create_lookup_file.py

--- Lists/PROXY/hideip.me/create_lookup_file.py
+++ Lists/PROXY/hideip.me/create_lookup_file.py
@@ -1,8 +1,9 @@
 import os
 import pandas as pd
+from collections import defaultdict
 
 def process_txt_files(output_csv='PROXY_ALL_hideip_me_List.csv'):
-    data = []
+    data = defaultdict(set)
     files = [f for f in os.listdir() if f.endswith('.txt')]
     
     for file in files:
@@ -11,11 +12,13 @@ def process_txt_files(output_csv='PROXY_ALL_hideip_me_List.csv'):
                 parts = line.strip().split(':')
                 if len(parts) == 3:
                     dest_ip, dest_port, dest_country = parts
-                    data.append((dest_ip, dest_port, dest_country, file))
+                    data[(dest_ip, dest_port, dest_country)].add(file)  # Collecting filenames in a set
     
-    # Create DataFrame and remove duplicates
-    df = pd.DataFrame(data, columns=['dest_ip', 'dest_port', 'dest_country', 'metadata_file'])
-    df.drop_duplicates(inplace=True)
+    # Convert collected data to a DataFrame
+    df = pd.DataFrame(
+        [(ip, port, country, ', '.join(sorted(files))) for (ip, port, country), files in data.items()],
+        columns=['dest_ip', 'dest_port', 'dest_country', 'metadata_file']
+    )
     
     # Save to CSV
     df.to_csv(output_csv, index=False, encoding='utf-8')
